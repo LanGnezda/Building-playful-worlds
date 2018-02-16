20d60713fc4f05a43376813ce8b9994f4e12b848
LanGnezda <36532684+LanGnezda@users.noreply.github.com>
2018-02-16 12:28:37 +0100

    the ting
    color toegevoegd als script

---
+++ Building playful worlds les 2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Color Objectcolor = new Color();
+    public Renderer objectRenderer;
+
+    // Use this for initialization
+    void Start () { 
+
+        objectRenderer.material.color = Objectcolor;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
